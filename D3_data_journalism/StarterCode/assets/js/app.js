b264a9f7e96656c1f60c01b6340d6363e502d0cb
issyl <issyl@comcast.net>
2021-05-18 23:36:59 -0400

    05.18.2021

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,35 @@
 // @TODO: YOUR CODE HERE!
+//Core Assignment: D3 Dabbler (Required Assignment)
+// Set up SVG definitions
+let svgWidth = 960;
+let svgHeight = 620;
+
+// set up borders in svg
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 200,
+    left: 100
+};
+
+// calculate chart height and width
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
+
+// append a div class to the scatter element
+let chart = d3.select('#scatter')
+    .append('div')
+    .classed('chart', true);
+
+//append an svg element to the chart 
+let svg = chart.append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+//append an svg group
+let chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+//initial parameters; x and y axis
+let chosenXAxis = 'poverty';
+let chosenYAxis = 'healthcare';
